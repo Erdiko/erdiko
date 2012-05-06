ce6e88d5ee347d76b022faeab541a7de4a57ea18
Dave LaFlam <dave@davelaflam.com>
2012-05-05 21:56:11 -0700

    - Adding jquery min file for rsvp theme, maybe this should be moved to a higher level - Added specific javascript logic to add/remove input fields based upon user select option - Updated rsvp.html view file for handle dynamically adding input fields - Temporarily added the jquery and rsvp .js files in the rsvp.html file, needs to be moved to footer.phtml

---
+++ www/app/theme/rsvp/js/rsvp.js
@@ -0,0 +1,13 @@
+(function() {
+
+  var scntDiv = $('#add_guests');
+
+  $('#num_guests').change(function() {
+    var inputFields = parseInt($('#num_guests').val(), 10);
+    scntDiv.empty()
+    for (var i = 0; i < inputFields; i++) {
+      scntDiv.append($('<p><label for="guest' + (i+1) + '">Guest ' + (i+1) + ' Name:</label> <input id="guest' + (i+1) + '" type="text" alt="Full Name" name="guest' + (i+1) + '" />'));
+    }
+  });
+
+})();
