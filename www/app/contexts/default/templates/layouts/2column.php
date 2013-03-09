867b0e363c3b525ef94a1625b3b3611bc707e278
John Arroyo <john.arroyo@gmail.com>
2013-03-08 22:59:17 -0800

    fixed classes in multi column layouts

--- www/app/contexts/default/templates/layouts/2column.php
+++ www/app/contexts/default/templates/layouts/2column.php
@@ -1,7 +1,7 @@
 <div class="unit size1of4 column-left">
   <?php echo $data->getSidebar('left'); ?>
 </div>
-<div class="right size3of4 column-main">
+<div class="unit size3of4 column-main">
 	<!-- Get main content -->
 	<?php echo $data->getMainContent(); ?>
 </div>
--- www/app/contexts/default/templates/layouts/3column.php
+++ www/app/contexts/default/templates/layouts/3column.php
@@ -1,9 +1,9 @@
-<div class="unit size1of4">
+<div class="unit size1of4 column-left">
   <?php echo $data->getSidebar('left'); ?>
 </div>
-<div class="unit size1of2">
+<div class="unit size1of2 column-main">
   <?php echo $data->getMainContent(); ?>
 </div>
-<div class="unit size1of4">
+<div class="unit size1of4 column-right">
   <?php echo $data->getSidebar('right'); ?>
 </div>
