66082d0db031c54446d6bd8344bfcc2d78a74a05
Varun Brahme <varun@arroyolabs.com>
2013-04-04 12:57:31 -0700

    removed extra log functions from the static class

--- www/Erdiko.php
+++ www/Erdiko.php
@@ -211,54 +211,9 @@ class Erdiko
 	 * @todo add log level as a number instead of a constant
 	 * @return 
 	 */
-	public static function log($logString,$logLevel=null)
+	public static function log($log,$logLevel=null)
 	{
 		$logObj=new \erdiko\core\Logger();
-		$logObj->log($logString,$logLevel);
+		$logObj->log($log,$logLevel);
 	}
-	
-	/**
-	 * logException
-	 * @usage Erdiko::logException('Sample exception');
-	 * @return 
-	 */
-	public static function logException($exception)
-	{
-		$logObj=new \erdiko\core\Logger();
-		$logObj->logException($exception);
-	}
-	
-	/**
-	 * logWarning
-	 * @usage Erdiko::logWarning('Sample warning');
-	 * @return 
-	 */
-	public static function logWarning($warning)
-	{
-		$logObj=new \erdiko\core\Logger();
-		$logObj->log($warning,Logger::WARNING);
-	}
-	
-	/**
-	 * logInfo
-	 * @usage Erdiko::logInfo('Sample info log');
-	 * @return 
-	 */
-	public static function logInfo($info)
-	{
-		$logObj=new \erdiko\core\Logger();
-		$logObj->log($info,Logger::INFO);
-	}
-	
-	/**
-	 * logNotice
-	 * @usage Erdiko::logNotice('Sample notice');
-	 * @return 
-	 */
-	public static function logNotice($notice)
-	{
-		$logObj=new \erdiko\core\Logger();
-		$logObj->log($notice,Logger::NOTICE);
-	}
-	
 }
