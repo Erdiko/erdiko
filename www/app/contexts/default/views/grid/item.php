dcd3673eb4bbaced994d5d5b8c4639a24870a93f
John Arroyo <john.arroyo@gmail.com>
2013-05-31 23:47:04 -0700

    made grid item more generic

--- www/app/contexts/default/views/grid/item.php
+++ www/app/contexts/default/views/grid/item.php
@@ -2,7 +2,7 @@
 <li class="unit size1of<?php echo $size; ?>">
   <figure>
     <div class="product-img-wrapper" data-index="0">
-      <a href="/artwork"><img class="product-img" src="http://cache.net-a-porter.com/images/products/312919/312919_in_mt2.jpg" alt="Run Techfit™ cropped leggings"></a>
+      <a href="<?php echo $data['details']['url']; ?>"><img class="product-img" src="http://placehold.it/180x180" alt="Grid Item"></a>
     </div>
     <figcaption>
       <h2 class="grid"><a href="<?php echo $data['details']['url']; ?>"><?php echo $data['details']['name']; ?></a></h2>
