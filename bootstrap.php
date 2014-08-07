be898f6136ee1f35487bfafd4e82232b33f08b5d
John Arroyo <john.arroyo@gmail.com>
2014-08-07 16:33:59 -0700

    remove includes so as to rely on the new autoloader

--- bootstrap.php
+++ bootstrap.php
@@ -19,23 +19,6 @@ require_once ROOT.'/Erdiko.php';
 require_once ERDIKO.'/Toro.php';
 require_once ERDIKO.'/core/autoload.php';
 
-// Temp hack loader @todo use composer's autoloader for core
-require_once ERDIKO.'/core/Controller.php';
-require_once ERDIKO.'/core/AjaxController.php';
-require_once ERDIKO.'/core/ModelAbstract.php';
-require_once ERDIKO.'/core/Response.php';
-require_once ERDIKO.'/core/AjaxResponse.php';
-require_once ERDIKO.'/core/Container.php';
-require_once ERDIKO.'/core/Theme.php';
-require_once ERDIKO.'/core/Layout.php';
-require_once ERDIKO.'/core/View.php';
-require_once ERDIKO.'/core/datasource/File.php';
-require_once ERDIKO.'/core/cache/CacheInterface.php';
-require_once ERDIKO.'/core/cache/File.php';
-require_once ERDIKO.'/core/Cache.php';
-require_once ERDIKO.'/core/Logger.php';
-require_once ERDIKO.'/core/cache/Memcached.php';
-
 // Composer
 require_once VENDOR.'/autoload.php';
 
