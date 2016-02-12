aa04c1648242e28a9ecb8c2569e6a61826c1e42a
Andy Armstrong <andyarmstro@gmail.com>
2016-02-11 21:15:20 -0800

    added assets to theme dir

--- app/themes/bootstrap/gulpfile.js
+++ app/themes/bootstrap/gulpfile.js
@@ -5,13 +5,22 @@ gulp.task('copy-assets', function() {
     var assets = {
         js: [
             './node_modules/angular/angular.js',
-            './node_modules/bootstrap/dist/js/bootstrap.min.js'
+            './node_modules/bootstrap/dist/js/bootstrap.min.js',
+            './js/carousel.js',
+            './js/home.js',
+            './js/default.js'
         ],
         css: [
-          './node_modules/bootstrap/dist/js/bootstrap.min.js'
+          './node_modules/bootstrap/dist/css/bootstrap.min.css',
+          './node_modules/bootstrap/dist/css/bootstrap-theme.min.css',
+          './css/carousel.css',
+          './css/home.css',
+          './css/default.css'
         ]
     };
     _(assets).forEach(function (assets, type) {
        gulp.src(assets).pipe(gulp.dest('../../../public/themes/bootstrap/' + type));
     });
 });
+
+gulp.task('default', ['copy-assets']);
