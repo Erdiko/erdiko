7c5c49bd70a53e29b49c0677a15fe147f3fbb6b3
John Arroyo <john.arroyo@gmail.com>
2016-03-07 13:02:32 -0800

    deleted some errant composer libs that made their way into the repo

--- vendor/erdiko/core/tests/bootstrap.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo dirname(dirname(dirname(dirname(__DIR__)))).'/bootstrap.php';
-require_once dirname(dirname(dirname(dirname(__DIR__)))).'/bootstrap.php'; // boot up Erdiko
--- vendor/mustache/mustache/.php_cs
+++
@@ -1,6 +0,0 @@
-<?php
-
-$config = new Symfony\CS\Config\Config();
-$config->getFinder()->in(__DIR__)->exclude('bin');
-
-return $config;
