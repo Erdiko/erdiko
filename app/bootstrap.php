5316c045aa2b669b0d5969cd9b3eba2ba4520b64
Leo Daidone <leodaido@gmail.com>
2016-10-26 21:40:31 -0300

    ER-51: added two templates for custom error shows, modified htaccess and nginx to redirect on http errors, modified bootstrap to fire custom views on error

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Define the project root and load up erdiko's core bootrap
- * You shouldn't have to change anything here unless you are 
+ * You shouldn't have to change anything here unless you are
  * hacking together something unique
  */
 define('ERDIKO_ROOT', dirname(__DIR__));
@@ -16,6 +16,7 @@ require_once ERDIKO_SRC.'/bootstrap.php';
  */
 ini_set('display_errors', '1');
 
+
 // To turn on Session (uncomment line below)
 // require_once ERDIKO.'/core/session.php';
 
@@ -23,38 +24,60 @@ ini_set('display_errors', '1');
  * Hooks
  */
 ToroHook::add("404", function ($vars = array()) {
-    // error_log("vars: ".print_r($vars, true));
-    
-    if (empty($vars['message'])) {
-        $vars['message'] = "Sorry, we cannot find that page.";
-    }
-    if (empty($vars['error'])) {
-        $vars['error'] = $vars['message'];
-    }
-    if (!isset($vars['path_info'])) {
-        $vars['path_info'] = "";
-    }
+	// Toro::serve Legacy
+	if (empty($vars['message'])) {
+		$vars['message'] = "Sorry, we cannot find that page.";
+	}
+	if (empty($vars['error'])) {
+		$vars['error'] = $vars['message'];
+	}
+	if (!isset($vars['path_info'])) {
+		$vars['path_info'] = "";
+	}
 
-    Erdiko::log(\Psr\Log\LogLevel::ERROR, "404 {$vars['path_info']} {$vars['error']}");
-
-    // For a simple text only 404 page use this...
-    // echo "Sorry, we cannot find that URL";
-    // die; // don't let the calling controller continue
-
-    // For a themed 404 page...
-    $theme = new \erdiko\core\Theme('bootstrap');
-    $theme->addCss('font-awesome', 
-        '//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css');
-    $theme->addCss('font-awesome-animation',
-        '/themes/bootstrap/css/font-awesome-animation.css');
-
-    $response = new \erdiko\core\Response($theme);
-    $response->setContent(\Erdiko::getView('404', $vars));
-    $response->send();
+	Erdiko::log(\Psr\Log\LogLevel::ERROR, "404 {$vars['path_info']} {$vars['error']}");
+	$vars['code'] = 404;
+	ToroHook::fire('server_error', $vars);
 });
 
 ToroHook::add("500", function ($vars = array()) {
-    // error_log("vars: ".print_r($vars, true));
-    Erdiko::log(\Psr\Log\LogLevel::ERROR, "500 {$vars['path_info']} {$vars['error']}");
-    echo "Sorry, something went wrong";
+	// Toro::serve Legacy
+	$vars['message'] = "500 {$vars['path_info']} {$vars['error']}";
+	$vars['code'] = 500;
+	ToroHook::fire('server_error', $vars);
 });
+
+// Error management & beautify output.
+ToroHook::add("server_error", function ($vars = array()) {
+	if(!isset($vars['path_info'])){
+		$vars['path_info'] = $_SERVER['REQUEST_URI'];
+	}
+	Erdiko::log(\Psr\Log\LogLevel::ERROR, "{$vars['code']}: {$vars['path_info']} {$vars['error']}");
+
+	$theme = new \erdiko\core\Theme('bootstrap');
+	$theme->addCss('font-awesome',
+		'//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css');
+	$theme->addCss('font-awesome-animation',
+		'/themes/bootstrap/css/font-awesome-animation.css');
+
+	$response = new \erdiko\core\Response($theme);
+	$response->setContent(\Erdiko::getView('server_error', $vars));
+	$response->send();
+});
+
+ToroHook::add("php_error", function ($vars = array()) {
+	Erdiko::log(\Psr\Log\LogLevel::ERROR, "{$vars['code']}: {$vars['path_info']} {$vars['error']}");
+
+	$theme = new \erdiko\core\Theme('bootstrap');
+	$theme->addCss('font-awesome',
+		'//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css');
+	$theme->addCss('font-awesome-animation',
+		'/themes/bootstrap/css/font-awesome-animation.css');
+
+	$response = new \erdiko\core\Response($theme);
+	$response->setContent(\Erdiko::getView('user_error', $vars));
+	$response->send();
+});
+
+// Error Management
+\erdiko\core\ErrorHandler::init();
--- public/default/index.php
+++ public/default/index.php
@@ -7,10 +7,17 @@
  * @package     Public
  * @copyright   Copyright (c) 2016, Arroyo Labs, www.arroyolabs.com
  * @author      John Arroyo
+ * @author      Leo Daidone
  */
 
 // Bootstrap your app and erdiko
 include_once dirname(dirname(__DIR__))."/app/bootstrap.php";
+
+$error = isset($_GET['error_code'])
+	? intval($_GET['error_code'])
+	: null;
+
+\erdiko\core\ErrorHandler::fire($error);
 
 try {
     $routes = Erdiko::getRoutes(getenv('ERDIKO_CONTEXT'));
