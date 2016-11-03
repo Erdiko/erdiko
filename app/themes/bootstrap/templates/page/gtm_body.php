9b65c9884eb893eed989edf1a896702d8642a170
Julian Diaz <julian@arroyolabs.com>
2016-11-03 18:27:02 -0300

    removed analitycs, now inside of google_body

--- app/themes/bootstrap/templates/page/gtm_body.php
+++
@@ -1,8 +0,0 @@
-<!-- Google Tag Manager (noscript) -->
-<noscript>
-    <iframe src="https://www.googletagmanager.com/ns.html?id=<?php echo $data['google_tag_manager']['container']['key'] ?>"
-            height="0" width="0" style="display:none;visibility:hidden">
-
-    </iframe>
-</noscript>
-<!-- End Google Tag Manager (noscript) -->
