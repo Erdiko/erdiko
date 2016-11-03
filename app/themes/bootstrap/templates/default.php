d047c08df05993bdbd23dd4b887d368e71fa9416
Julian Diaz <julian@arroyolabs.com>
2016-11-03 18:30:16 -0300

    modified file names and removed call to analytics

--- app/themes/bootstrap/templates/default.php
+++ app/themes/bootstrap/templates/default.php
@@ -19,7 +19,7 @@ foreach ($data->getCss() as $css) {
         echo "<link rel='stylesheet' href='".$css['file']."' type='text/css' />\n";
     }
 }
-echo $this->getTemplateHtml('gtm_header');
+echo $this->getTemplateHtml('google_header');
 ?>
 </head>
 <body>
@@ -46,8 +46,7 @@ $(document).ready(function() {
 });
 /* ]]> */</script>
 
-<?php echo $data->getTemplateHtml('analytics') ?>
-<?php echo $this->getTemplateHtml('gtm_body') ?>
+<?php echo $this->getTemplateHtml('google_body') ?>
 
 </body>
 </html>
