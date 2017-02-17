ef9016ae6db6942d130e4e6c9255311a73912006
Andy Armstrong <andyarmstro@gmail.com>
2017-02-17 13:35:12 -0800

    fixed file name

--- docker/php/7.0/scripts/phpunit.sh
+++ docker/php/7.0/scripts/phpunit.sh
@@ -2,6 +2,6 @@
 
 # Install Phpunit, https://phpunit.de
 wget https://phar.phpunit.de/phpunit-5.7.phar
-chmod +x phpunit.phar
-mv phpunit.phar /usr/local/bin/phpunit
+chmod +x phpunit-5.7.phar
+mv phpunit-5.7.phar /usr/local/bin/phpunit
 phpunit --version
