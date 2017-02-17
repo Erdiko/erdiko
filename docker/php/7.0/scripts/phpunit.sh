c65c8ab3e2cd9a862f3e95ea508b89212e523347
Andy Armstrong <andyarmstro@gmail.com>
2017-02-17 13:30:39 -0800

    Make sure we use phpunit 5.7

--- docker/php/7.0/scripts/phpunit.sh
+++ docker/php/7.0/scripts/phpunit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Install Phpunit, https://phpunit.de
-wget https://phar.phpunit.de/phpunit.phar
+wget https://phar.phpunit.de/phpunit-5.7.phar
 chmod +x phpunit.phar
 mv phpunit.phar /usr/local/bin/phpunit
 phpunit --version
