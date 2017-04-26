089062078f403b4ee0ef99b6361982d0ec777bad
john arroyo <john.arroyo@gmail.com>
2017-04-26 01:37:23 -0700

    leverage new erdiko App

--- public/index.php
+++ public/index.php
@@ -10,15 +10,11 @@
 
 // Bootstrap erdiko & composer autoload
 define('ERDIKO_ROOT', dirname(__DIR__));
-require ERDIKO_ROOT . '/vendor/erdiko/core/bootstrap.php';
+require dirname(__DIR__) . '/vendor/erdiko/core/bootstrap.php';
 session_start(); // @todo put somewhere else, startSession()
 
-// Load app settings
-$settings = require ERDIKO_ROOT."/contexts/".getenv('ERDIKO_CONTEXT')."/bootstrap/settings.php";
-// $settings =  \erdiko\App::getSettings(); // getSettings()
-
 // Instantiate the app
-$app = new \Slim\App($settings);
+$app = new \erdiko\App();
 
 require ERDIKO_ROOT."/contexts/".getenv('ERDIKO_CONTEXT')."/bootstrap.php";
 
