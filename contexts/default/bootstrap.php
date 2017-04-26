83b8625c76520987e8b12e93dbd1affb59da66f5
john arroyo <john.arroyo@gmail.com>
2017-04-25 22:43:55 -0700

    add default context

---
+++ contexts/default/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+// Set up dependencies
+require_once __DIR__ . '/dependencies.php';
+
+// Register middleware
+require_once __DIR__ . '/middleware.php';
+
+// Register routes
+require_once __DIR__ . '/routes.php';
