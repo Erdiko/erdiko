36f91b053461bc6e9732aae6065561c7dc739a87
john arroyo <john.arroyo@gmail.com>
2017-04-26 01:58:27 -0700

    add some comments

--- public/index.php
+++ public/index.php
@@ -8,15 +8,16 @@
  * @author      John Arroyo <john@arroyolabs.com>
  */
 
-// Bootstrap erdiko & composer autoload
-define('ERDIKO_ROOT', dirname(__DIR__));
-require dirname(__DIR__) . '/vendor/erdiko/core/bootstrap.php';
-session_start(); // @todo put somewhere else, startSession()
+// Bootstrap erdiko (& composer)
+define('ERDIKO_ROOT', dirname(__DIR__)); // @todo move, perhaps use a setenv?
+require ERDIKO_ROOT . '/vendor/erdiko/core/bootstrap.php';
+require ERDIKO_ROOT . '/vendor/erdiko/core/session.php'; // @todo put somewhere else?
 
-// Instantiate the app
+// Instantiate the app (autoloads the context settings)
 $app = new \erdiko\App();
 
+// Bootstrap your app (context)
 require ERDIKO_ROOT."/contexts/".getenv('ERDIKO_CONTEXT')."/bootstrap.php";
 
-// Run the app
+// Run the app (autoloads the context bootstrap)
 $app->run();
