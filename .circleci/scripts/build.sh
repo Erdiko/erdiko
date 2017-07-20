bc45460f3568f6331cc5f8654441b09308b7149b
john arroyo <john.arroyo@gmail.com>
2017-07-20 10:02:34 -0700

    simple build script

---
+++ .circleci/scripts/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# clone repos
+git clone git@github.com:Erdiko/erdiko.git -b erdiko2
+git clone git@github.com:Erdiko/core.git -b erdiko2
+git clone git@github.com:Erdiko/theme.git
+
+cd erdiko
+docker-compose up &
+
+# To install packages connect to the erdiko php container and run composer install or composer update
+bash -c "clear && docker exec -it erdiko_php /bin/bash"
+cd /code
+composer update
