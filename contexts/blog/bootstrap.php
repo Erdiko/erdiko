57d8ed3f3cbe8fa410962d16cb6b58d2c386444c
john arroyo <john.arroyo@gmail.com>
2017-09-12 00:58:13 -0700

    delete all contexts

--- contexts/blog/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Set up dependencies
-require_once __DIR__ . '/bootstrap/dependencies.php';
-
-// Register middleware
-require_once __DIR__ . '/bootstrap/middleware.php';
-
-// Register routes
-require_once __DIR__ . '/bootstrap/routes.php';
