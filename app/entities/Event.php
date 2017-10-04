db7208649c348f856c78111d8e992a27448c7a52
kkennethlee <kyung.kenneth.lee@gmail.com>
2017-10-03 18:01:20 -0700

    created entities

---
+++ app/entities/Event.php
@@ -0,0 +1,140 @@
+<?php
+namespace app\entities;
+
+/**
+ * @Entity @Table(name="event")
+ */
+class Event
+{
+    /**
+     * @Id @GeneratedValue @Column(type="integer")
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     * @Column(type="integer")
+     * @var integer
+     */
+    protected $users_id;
+
+
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    protected $description;
+
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    protected $template;
+
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    protected $value_unit;
+
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    protected $created_at;
+
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    protected $updated_at;
+
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    protected $event_table_name;
+
+
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getUsersId($users_id)
+    {
+        return $this->users_id;
+    }
+
+    public function setUsersId($users_id)
+    {
+        $this->users_id = $users_id;
+    }
+
+    public function getDescription($description)
+    {
+        return $this->description;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    public function getTemplate($template)
+    {
+        return $this->template;
+    }
+
+    public function setTemplate($template)
+    {
+        $this->template = $template;
+    }
+
+    public function getValueUnit($value_unit)
+    {
+        return $this->value_unit;
+    }
+
+    public function setValueUnit($value_unit)
+    {
+        $this->value_unit = $value_unit;
+    }
+
+    public function getCreatedAt($created_at)
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt($created_at)
+    {
+        $this->created_at = $created_at;
+    }
+
+    public function getUpdatedAt($updated_at)
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt($updated_at)
+    {
+        $this->updated_at = $updated_at;
+    }
+
+    public function getEventTableName($event_table_name)
+    {
+        return $this->event_table_name;
+    }
+
+    public function setEventTableName($event_table_name)
+    {
+        $this->event_table_name = $event_table_name;
+    }
+
+}
---
+++ app/entities/EventValueNumber.php
@@ -0,0 +1,89 @@
+<?php
+namespace app\entities;
+
+/**
+ * @Entity @Table(name="event_value_number")
+ */
+ class EventValueNumber
+ {
+    /**
+     * @Id @GeneratedValue @Column(type="integer")
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     * @Column(type="integer")
+     * @var integer
+     */
+    protected $event_id;
+
+    /**
+     * @Column(type="integer")
+     * @var integer
+     */
+    protected $users_id;
+
+    /**
+     * @Column(type="integer")
+     * @var integer
+     */
+    protected $value; //Float
+
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    protected $created_at;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getEventId($event_id)
+    {
+        return $this->event_id;
+    }
+
+    public function setEventId($event_id)
+    {
+        $this->event_id = $event_id;
+    }
+
+    public function getUsersId($users_id)
+    {
+        return $this->users_id;
+    }
+
+    public function setUsersId($users_id)
+    {
+        $this->users_id = $users_id;
+    }
+
+    public function getValue($value)
+    {
+        return $this->value;
+    }
+
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+
+    public function getCreatedAt($created_at)
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt($created_at)
+    {
+        $this->created_at = $created_at;
+    }
+
+ }
